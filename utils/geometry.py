fb496c6f2bcad0c9fe0ca62d9986eb22d0ebf4ff
Karo Antonio <karoantonio@gmail.com>
2017-03-23 14:33:18 -0400

    lets get it started in ha

---
+++ utils/geometry.py
@@ -0,0 +1,20 @@
+
+
+def construct_geometry(edges):
+	'''
+	edges: [(v1,v2),(v3,v4),...]  tuples of vertices , 
+									the vertices may or may not be unique
+
+	For the given vertices and edges, 
+		determine the shape that each set of edges belongs too,
+		fill in missing edges and vertices
+		
+	return geometries: [(vertices,edges),...]
+	'''
+	pass
+
+def geometries_to_obj(geoms):
+	pass
+
+
+
